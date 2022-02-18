41987b4590a218dd8186295c10065df9a14ebe56
Flavie Magne <flavie.magne@reseau.eseo.fr>
2022-02-18 20:53:05 +0100

    webpage + converter

---
+++ converter.py
@@ -0,0 +1,19 @@
+cpp_lines = []
+
+html_file = open('./index.html')
+html_lines = html_file.readlines()
+
+for line in html_lines:
+    if line=="":
+        pass
+    else:
+        line = line.replace('"', "'")
+        line = line.strip()
+        line='client.println("'+line
+        line=line+'");'
+        line=line+"\n"
+        cpp_lines.append(line)
+
+outfile = open('./script.ino', 'w')
+outfile.writelines(cpp_lines)
+outfile.close()
